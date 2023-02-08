a4d95be7911d8e3ec318d9c3cdf9ba64cbc9d5fd
Zach <zacharymvig@gmail.com>
2023-02-08 10:37:33 -0600

    Added correct HySpec Image Processing File
    Deleted old files and uploaded new ones

--- HySpec_Image_Processing.py
+++ HySpec_Image_Processing.py
@@ -4,6 +4,10 @@ Created on Wed Feb  1 10:34:52 2023
 
 @author: zacha
 """
+##Timing the program
+import time
+start = time.time()
+
 ##Importing Necessary Libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -24,11 +28,15 @@ import tifffile as tf
 from scipy import interpolate as interp
 #from Linear_Spline import linear_spline
 
-def get_hdr_files():
+def get_hdr_files(select):
     ##Asks user what the source directory is for M3 Data
     Tk().withdraw()
-    hdr_folder_path = askdir()
-    #hdr_folder_path = r"D:\Data\20230131T17554836251"
+    if select == True:    
+        hdr_folder_path = askdir()
+    elif select == False:
+        hdr_folder_path = r"D:\Data\20230131T17554836251"
+    else:
+        raise Exception("Select is either True or False")
     
     
     ##Unzips files and places them in "extracted_files" folder in source directory
@@ -100,6 +108,10 @@ class HDR_Image():
     ##String print method
     def __str__(self):
         return f"HDR Image Class: {self.hdr.fid.name[find(self.hdr.fid.name,'/')[-1]+1:len(self.hdr.fid.name)-4]}"
+    
+    ##Returns HDR Object for testing purposes
+    def hdr(self):
+        return self.hdr
     
     ##Method that returns date, time, and data types of HDR Image
     def datetime(self):
@@ -224,26 +236,107 @@ class HDR_Image():
     
     
     ## Plots spectrum of a given x,y point of HDR Image
-    def plot_spec(self,x,y):
-        wvl = self.hdr.bands.centers
-        spec_list = []
-        wvl_list = []
-        for data_val,wavelength in zip(self.hdr.read_pixel(x,y),wvl):
-            if abs(data_val)<500 and wavelength<2500 and wavelength>1000:
-                spec_list.append(data_val)
-                wvl_list.append(wavelength)
-        plt.plot(wvl_list,spec_list,alpha=0.6,ls='--')
+    def plot_spec(self,x,y,**kwargs):
+        if self.hdr.bands.centers != None:
+            fig,ax = plt.subplots(1,1)
+            
+            wvl = self.hdr.bands.centers
+            spec_list = []
+            wvl_list = []
+            for data_val,wavelength in zip(self.hdr.read_pixel(x,y),wvl):
+                if abs(data_val)<500 and wavelength<2500 and wavelength>1000:
+                    spec_list.append(data_val)
+                    wvl_list.append(wavelength)
+                    
+            if kwargs["plot_og"] == True:
+                ax.plot(wvl_list,spec_list,alpha=0.6,ls='--',label='Original Spectrum')
+            else:
+                pass
+            
+            f = interp.CubicSpline(np.array(wvl_list),np.array(spec_list))
+            x = np.linspace(min(wvl_list),max(wvl_list),30)
+            
+            if kwargs["plot_cspline"] == True:
+                ax.plot(x,f(x),'-',color="orange",label='Cubic Spline')
+            else:
+                pass
+
+            
+            try:
+                if kwargs["filter_img"] == True:
+                    if np.all(np.array(spec_list)>0.1) == True:
+                        print (np.array(spec_list))
+                    plt.close()
+                elif kwargs["filter_img"] == False:
+                    plt.show()
+            except:
+                pass
+                
+            box_size = int(input("Box Size: "))
+            avg_array = np.zeros((box_size,2))
+            avg_rfl = []
+            std_rfl = []
+            avg_wvl = []
+            n = 1
+            for spec,wvl in zip(spec_list,wvl_list):
+                if n%box_size != 0:
+                    avg_array[n-1] = (spec,wvl)
+                    n+=1
+                elif n%box_size == 0:
+                    avg_array[n-1] = (spec,wvl)
+                    avg_rfl.append(np.average(avg_array[:,0]))
+                    std_rfl.append(np.std(avg_array[:,0]))
+                    avg_wvl.append(np.average(avg_array[:,1]))
+                    avg_array = np.zeros((box_size,2))
+                    n=1
+            
+            rfl_last = []
+            wvl_last = []
+            for rfl,wvl in zip(avg_array[:,0],avg_array[:,1]):
+                if rfl > 0:
+                    rfl_last.append(rfl)
+                    wvl_last.append(wvl)
+                    
+            
+            avg_rfl.append(np.average(rfl_last))
+            std_rfl.append(np.std(rfl_last))
+            avg_wvl.append(np.average(wvl_last))
+
+            if kwargs["plot_boxcar"] == True:
+                ax.plot(avg_wvl,avg_rfl,c='red',label=f'Boxcar({box_size} pts.)')
+                ax.fill_between(avg_wvl,np.array(avg_rfl)+np.array(std_rfl),
+                                np.array(avg_rfl)-np.array(std_rfl),alpha=0.1,color='red')
+            else: 
+                pass
+            
+            f1 = interp.CubicSpline(np.array(avg_wvl),np.array(avg_rfl))
+            f1_err = interp.CubicSpline(np.array(avg_wvl),np.array(std_rfl))
+            x1 = np.linspace(min(wvl_list),max(wvl_list),30)
+
+            if kwargs["plot_cspline_boxcar"] == True:
+                ax.plot(x1,f1(x1),c='black',ls='dashdot',label=f'Boxcar({box_size} pts.)  & Cubic Spline')
+                ax.fill_between(x1,f1(x1)+f1_err(x1),f1(x1)-f1_err(x1),alpha=0.1,color='black')
+            else:
+                pass
+                    
+            #ax.plot(wvl,avg_rfl,c="red")    
+            ax.set_xlabel('Wavelength (\u03BCm)')
+            ax.set_ylabel('Reflectance')
+            
+            ax.legend()
+            
+            #return spec_list,wvl_list
+            
+        elif self.hdr.bands.centers == None:
+            spec_list,wvl_list = [],[]
+            return spec_list,wvl_list
         
-        f = interp.make_interp_spline(wvl_list,spec_list,k=3)
-        
-        x = np.linspace(min(wvl_list),max(wvl_list),30)
-        
-        plt.plot(x,f(x),'-',color="orange")
-        plt.xlabel('Wavelength (\u03BCm)')
-        plt.ylabel('Reflectance')
-    
-        
-    ## Plots image of parameter 1
+    def good_spectra(self):
+        bands = 1
+        for i in range(0,self.hdr.nbands-1):
+            band = self.hdr.read_band(i)
+            bands+=1
+        print (f'There are {bands} bands and {band.shape[0]*band.shape[1]} pixels')
     def H2O_p1(self):
         wv1 = self.hdr.read_band(10)
         wv2 = self.hdr.read_band(12)
@@ -256,20 +349,39 @@ class HDR_Image():
         plt.yticks([])
         plt.show()
         
-hdr_file_list,hdr_folder_list = get_hdr_files()
+hdr_file_list,hdr_folder_list = get_hdr_files(False)
 
 obj_list = []
 for path,file in zip(hdr_folder_list,hdr_file_list):
-    obj_list.append(HDR_Image(path+'/'+file))
+    if file.find('rfl') > -1:
+        obj_list.append(HDR_Image(path+'/'+file))
 
-for obj in obj_list:
-    obj.datetime()
-    
-    
-wv1 = obj_list[0].plot_spec(90,103)    
+# =============================================================================
+# for obj in obj_list:
+#     obj.datetime()
+# =============================================================================
+
+# =============================================================================
+# for obj in obj_list:
+#     spec1,wv1 = obj.plot_spec(90,103)
+# =============================================================================
 #obj_list[0].wavelengths()
 
-#obj_list[0].plt_img(1289.41)
+# =============================================================================
+# pixels = 1
+# for x in range(1015,obj_list[0].hdr.nrows):
+#     for y in range(302,obj_list[0].hdr.ncols):
+#         spec,wvl = obj_list[0].plot_spec(x,y,filter_img=True)
+#         print (f'{pixels} out of {obj_list[0].hdr.nrows*obj_list[0].hdr.ncols} pixels processed')
+#         pixels += 1
+# =============================================================================
+        
+#obj_list[0].plot_spec(100,100,plot_og=True,plot_boxcar=False,plot_cspline=False,plot_cspline_boxcar=True)
+
+# =============================================================================
+# for obj in obj_list:
+#     obj.good_spectra()
+# =============================================================================
 
 # =============================================================================
 # for obj in obj_list:
@@ -278,3 +390,12 @@ wv1 = obj_list[0].plot_spec(90,103)
 
 
 
+end = time.time()
+runtime = end-start
+if runtime < 1:
+    print (f'Program Executed in {runtime*10**3:.3f} milliseconds')
+elif runtime < 60 and runtime > 1:
+    print (f'Program Executed in {runtime:.3f} seconds')
+elif runtime > 60:
+    print (f'Program Executed in {runtime/60:.1f} minutes and {runtime%60:.3f} seconds')
+    
